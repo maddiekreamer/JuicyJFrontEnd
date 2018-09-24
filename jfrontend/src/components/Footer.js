e0060c5cd0901116f10fb389c50cb6836c3b5ec1
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-09-24 13:24:38 -0600

    Added Header and Footer components

---
+++ jfrontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>
+        &copy; Juicy J-Mart 2018
+      </small>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ jfrontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Just a Test</h1>
+    </header>
+  );
+}
+
+export default Header;
