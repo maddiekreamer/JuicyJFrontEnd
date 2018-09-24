0ea8e77a038fb132f630d70179c588ff8b23434e
Damon Chivers <chiversdamon@gmail.com>
2018-09-24 15:31:40 -0600

    adding to Header component logo and search bar

--- jfrontend/src/components/Header.js
+++ jfrontend/src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../App.css"
+
+
 
 const Header = () => {
   return (
-    <header>
-      <h1>Just a Test</h1>
+    <header className="Header-head">
+      <Container id="Header-container">
+        <Row>
+          <Col md="6"><img src={require("../Images/JuicyJlight.png")} id="Header-img" /></Col>
+          <Col md="6" id="Header-search"><input type="text" id="Header-input" placeholder="Search"/></Col>
+        </Row>
+      </Container>
     </header>
   );
 }
