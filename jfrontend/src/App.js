c706be9c669ca407d8c43f5316b3f0d4ad894edf
Damon Chivers <chiversdamon@gmail.com>
2018-09-25 10:39:52 -0600

    Created fetch request and constructor function. Passed data prop to main component.

--- jfrontend/src/App.js
+++ jfrontend/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./components/Main"
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+  
+  componentDidMount() {
+    this.getData()
+  }  
+
+    getData = () => {
+    fetch() 
+    .then(result => result.json())
+    .then(result => this.setState({
+      data: result
+    }))
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-
+        <Main data = {this.state.data} />
         <Footer />
       </div>
     );
