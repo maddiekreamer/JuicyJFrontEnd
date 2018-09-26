598ec1a522633e5dfab001dee48e7faec40b1e72
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-09-25 21:25:13 -0600

    Reverse changes to index.js

--- jfrontend/src/index.js
+++ jfrontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Client from "../src/components/Client.js";
 
-ReactDOM.render(<Client />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
