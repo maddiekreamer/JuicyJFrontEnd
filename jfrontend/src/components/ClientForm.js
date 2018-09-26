b31f29b235c18e0736049d8e4e5045987431f4c6
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-09-26 17:22:48 -0600

    Add nick code and add buttons

--- jfrontend/src/components/ClientForm.js
+++ jfrontend/src/components/ClientForm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {Component} from "react";
 import SubmitButton from "./SubmitButton";
 import {Col, Form, FormGroup, Label, Input, Button} from "reactstrap";
@@ -65,63 +64,21 @@ class ClientForm extends Component {
             <Label for="description" size="sm">Description</Label>
             <Input type="textarea" name="description" id="description" bsSize="sm" ref="description" value={this.state.description}/>
           </FormGroup>
-          <SubmitButton />
+          <Button>
+            Add New Product
+          </Button>
+          <Button>
+            Update Product Info
+          </Button>
+          <Button>
+            Delete
+          </Button>
+
         </Form>
 
     )
   }
   
-=======
-import React from "react";
-import {Col, Form, FormGroup, Label, Input, Button} from "reactstrap";
-import "../App.css";
-
-const ClientForm = (props) => {
-
-  return (
-      <Form className="m-5">
-        <h6>Add New Product</h6>
-        <FormGroup row>
-          <Col sm="8">
-            <Label for="product" size="sm">Product</Label>
-            <Input type="text" name="product" id="product" placeholder="Product" bsSize="sm"/>
-          </Col>
-          <Col sm="2">
-            <Label for="quantity" size="sm">Quantity</Label>
-            <Input type="number" name="quantity" id="quantity" placeholder="Quantity" bsSize="sm"/>
-          </Col>
-          <Col sm="2">
-            <Label for="category" size="sm">Category</Label>
-            <Input type="number" name="category" id="category" placeholder="Category" bsSize="sm"/>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Col sm="9">
-            <Label for="url" size="sm">Image</Label>
-            <Input type="url" name="url" id="url" placeholder="Image url" bsSize="sm"/>
-          </Col>
-          <Col sm="3">
-            <Label for="price" size="sm">Price</Label>
-            <Input type="text" name="price" id="price" placeholder="Price" bsSize="sm"/>
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Label for="description" size="sm">Description</Label>
-          <Input type="textarea" name="description" id="description" bsSize="sm"/>
-        </FormGroup>
-        <Button size="sm">
-          Submit New Product
-        </Button>
-        <Button size="sm">
-          Update Product Info
-        </Button>
-        <Button size="sm">
-          Delete
-        </Button>
-      </Form>
-
-  )
->>>>>>> f0aa9052a062c1e19d0ceb18eecb5d6199da3633
 }
 
 export default ClientForm;
