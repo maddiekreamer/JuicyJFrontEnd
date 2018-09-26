7d7f8569c55044c66b41cc4de4627d59b2850e45
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-25 18:14:49 -0600

    added categories component that changes data key in state to include only items of specified category

--- jfrontend/src/App.js
+++ jfrontend/src/App.js
@@ -8,16 +8,36 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: []
+      data: [],
+      categories: []
     }
   }
 
   componentDidMount() {
     this.getData()
-  }  
+    this.getCategories()
+  }
 
-    getData = () => {
+  
+
+  getData = () => {
     fetch("https://j-j-data.herokuapp.com/") 
+    .then(result => result.json())
+    .then(result => this.setState({
+      data: result.data
+    }))
+  }
+
+  getCategories = () => {
+    fetch(`http://j-j-data.herokuapp.com/categories`)
+    .then(result => result.json())
+    .then(result => this.setState({
+      categories: result.data
+    }))
+  }
+
+  selectCategory = (id) => {
+    fetch(`http://j-j-data.herokuapp.com/${id}`)
     .then(result => result.json())
     .then(result => this.setState({
       data: result.data
@@ -26,10 +46,14 @@ class App extends Component {
 
   render() {
     console.log(this.state.data)
+    console.log(this.state.categories)
     return (
       <div className="App">
         <Header />
-        <Main data = {this.state.data} />
+        <Main data = {this.state.data}
+              categories={this.state.categories}
+              selectCategory={this.selectCategory}
+              getData={this.getData} />
         <Footer />
       </div>
     );
--- jfrontend/src/components/Main.js
+++ jfrontend/src/components/Main.js
@@ -1,36 +1,36 @@
 import React, {Component} from "react";
-import { Nav, NavItem, NavLink, Container, Row, Col} from 'reactstrap';
+import { Nav, NavItem, NavLink, Container, Button, ButtonGroup, Row, Col} from 'reactstrap';
 import Cards from "./Cards";
+import Categories from "./Categories";
 
 function Main(props) {
 
   const createCard = props.data.map((list, i) => {
     console.log('message',props.data)
-      return (
-        <Cards list={list} key={i}/>
-      )
-    })
+    return (
+      <Cards list={list} key={i}/>
+    )
+  })
+  
+  const createCategories = props.categories.map((type, i) =>  {
+    return  (
+      <Categories category={type}
+                  key={i}
+                  selectCategory={props.selectCategory} />
+    )
+  })
   
     return (
       <main>
           <Container>
             <Row>
               <Col sm="3" className="Main-nav">
-                <Nav vertical className="text-center">
+                <ButtonGroup vertical>
                   <p>Product Categories</p>
-                  <NavItem>
-                    <NavLink className="Main-nav-link" href="#">Snacks</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className="Main-nav-link" href="#">Drinks</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className="Main-nav-link" href="#">Specials</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className="Main-nav-link" href="#">Contact</NavLink>
-                  </NavItem>
-                </Nav>
+                  {createCategories}
+                  <Button onClick={(event) => props.getData()}>All</Button>
+                  <Button>Contact</Button>
+                </ButtonGroup>
               </Col>
               <Col sm="9" className="bg-warning">
                 <Container>
